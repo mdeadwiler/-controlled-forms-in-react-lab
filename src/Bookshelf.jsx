645c3c2dd463c12c2602bc4078a26f3696420254
MDeadwiler <mdeadwiler@email.com>
2024-10-14 19:24:12 -0400

    controlled-forms-in-react-lab

---
+++ src/Bookshelf.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+const Bookshelf = () => {
+    //This defines the state
+    const [books, setBooks] = useState([]);
+    const [newBook, setNewBook] = useState({title: '', author: ''});
+}
+    <div className="bookshelfDiv">
+  <div className="formDiv">
+    <h3>Add a Book</h3>
+    {/* Form will go here */}
+  </div>
+  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+</div>
+  
+  
+  export default Bookshelf;
+  
+  
